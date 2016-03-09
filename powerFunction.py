c0c193383e4769e1ce948b5fab3b09144bc10a84
Christine <Christine@Christines-Air.home>
2016-03-09 18:57:19 -0500

    Working power function program

--- powerFunction.py
+++ powerFunction.py
@@ -8,6 +8,23 @@ def power(base,exponent):
 		answer = answer * base 
 	return answer
 
+#---Main Program---
+
+base = int(raw_input("Enter a base number: "))
+exponent = int(raw_input("Enter an exponent number: "))
+
+#special case
+if base == 0 and exponent == 0: 
+	print("That is undefined.")
+	exit()
+	
+if exponent == 0: 
+	print("Your answer is 1") #anything exponent 0 = 1
+
+else: #normal operations
+	answer = power(base,exponent) #calling the function
+	print("Your answer is {}.".format(answer))
+
 #Need a loop
 #need a condition when multiply times == exponent then stop and then 
 #print the result in the main program
